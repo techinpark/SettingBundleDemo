bad327001c370367c46c360ee9f6de934c41ffff
Fernando <fernando@kakao.com>
2020-07-29 01:02:58 +0900

    Initial Commit

--- SettingBundleDemo/ViewController.swift
+++ SettingBundleDemo/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        for (key, value) in UserDefaults.standard.dictionaryRepresentation() {
+            print("\(key) = \(value) \n")
+        }
     }
 
 
